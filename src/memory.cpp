6e40a94492721893c451bfe7de36b602a3adfd43
Jack Karamanian <karamanian.jack@gmail.com>
2019-01-17 18:49:53 -0600

    add a System member to Memory, move functions to cpp file

---
+++ src/memory.cpp
@@ -0,0 +1,17 @@
+#include "cpu.h"
+
+namespace gb {
+void Memory::set(const u16& addr, const u8& val) {
+  if (addr == 0xff46) {
+    do_dma_transfer(val);
+  }
+  memory[addr] = val;
+}
+
+void Memory::do_dma_transfer(const u8& data) {
+  const u16 addr = ((u16)data) << 8;
+  for (u16 i = 0; i < 160; i++) {
+    memory[0xfe00 + i] = memory[addr + i];
+  }
+}
+}  // namespace gb
--- src/memory.h
+++ src/memory.h
@@ -1,21 +1,20 @@
 #pragma once
+#include "system.h"
 #include "types.h"
 
 namespace gb {
 struct Memory {
+  const System* system;
+  Memory(const System* system) : system{system} {}
   u8 memory[0xFFFF];
 
-  template<typename T = u8>
-  inline T* at(const u16 &addr) {
-    return reinterpret_cast<T *>(&memory[addr]);
-  } 
-
-  void set(const u16 &addr, const u8 &val) {
-    memory[addr] = val;
+  template <typename T = u8>
+  inline T* at(const u16& addr) {
+    return reinterpret_cast<T*>(&memory[addr]);
   }
 
+  void set(const u16& addr, const u8& val);
 
-
-
+  void do_dma_transfer(const u8& val);
 };
-}
+}  // namespace gb
