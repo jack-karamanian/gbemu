7442a14ee3bc65ab87f449bdec618a5250398fdc
Jack Karamanian <karamanian.jack@gmail.com>
2019-01-26 09:52:26 -0600

    only allow addresses >= 0x8000 to be written to

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "memory.h"
 
 namespace gb {
@@ -5,7 +6,14 @@ void Memory::set(const u16& addr, const u8& val) {
   if (addr == 0xff46) {
     do_dma_transfer(val);
   }
-  memory[addr] = val;
+
+  if (addr == 0xff02 && val == 0x81) {
+    std::cout << "char: " << memory[0xff01] << std::endl;
+  }
+
+  if (addr >= 0x8000) {
+    memory[addr] = val;
+  }
 }
 
 void Memory::do_dma_transfer(const u8& data) {
@@ -13,5 +21,9 @@ void Memory::do_dma_transfer(const u8& data) {
   for (u16 i = 0; i < 160; i++) {
     memory[0xfe00 + i] = memory[addr + i];
   }
+}
+
+u8* Memory::get_input_register() {
+  return &memory[0xff00];
 }
 }  // namespace gb
