e3bdc493283d038330c0328d8e9c4d3f590011e0
Jack Karamanian <karamanian.jack@gmail.com>
2019-01-27 16:17:51 -0600

    refactor Memory::at to return value types, use spans for memory ranges, add reset function, add load_rom function, add get_sprite_attributes function

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,7 +1,13 @@
+#include <algorithm>
 #include <iostream>
 #include "memory.h"
 
 namespace gb {
+nonstd::span<const u8> Memory::get_range(std::pair<u16, u16> range) {
+  const auto [begin, end] = range;
+
+  return nonstd::span<const u8>(&memory.at(begin), &memory.at(end + 1));
+}
 void Memory::set(const u16& addr, const u8& val) {
   if (addr == 0xff46) {
     do_dma_transfer(val);
@@ -14,6 +20,44 @@ void Memory::set(const u16& addr, const u8& val) {
   if (addr >= 0x8000) {
     memory[addr] = val;
   }
+}
+
+void Memory::reset() {
+  memory[0xFF05] = 0x00;
+  memory[0xFF06] = 0x00;
+  memory[0xFF07] = 0x00;
+  memory[0xFF10] = 0x80;
+  memory[0xFF11] = 0xBF;
+  memory[0xFF12] = 0xF3;
+  memory[0xFF14] = 0xBF;
+  memory[0xFF16] = 0x3F;
+  memory[0xFF17] = 0x00;
+  memory[0xFF19] = 0xBF;
+  memory[0xFF1A] = 0x7F;
+  memory[0xFF1B] = 0xFF;
+  memory[0xFF1C] = 0x9F;
+  memory[0xFF1E] = 0xBF;
+  memory[0xFF20] = 0xFF;
+  memory[0xFF21] = 0x00;
+  memory[0xFF22] = 0x00;
+  memory[0xFF23] = 0xBF;
+  memory[0xFF24] = 0x77;
+  memory[0xFF25] = 0xF3;
+  memory[0xFF26] = 0xF1;
+  memory[0xFF40] = 0x91;
+  memory[0xFF42] = 0x00;
+  memory[0xFF43] = 0x00;
+  memory[0xFF45] = 0x00;
+  memory[0xFF47] = 0xFC;
+  memory[0xFF48] = 0xFF;
+  memory[0xFF49] = 0xFF;
+  memory[0xFF4A] = 0x00;
+  memory[0xFF4B] = 0x00;
+  memory[0xFFFE] = 0x00;
+}
+
+void Memory::load_rom(const std::vector<u8>& data) {
+  std::copy(data.begin(), data.end(), &memory[0]);
 }
 
 void Memory::do_dma_transfer(const u8& data) {
@@ -29,5 +73,14 @@ const u8* Memory::get_input_register() {
 
 void Memory::set_input_register(u8 val) {
   set(0xff00, val);
+}
+
+nonstd::span<const SpriteAttribute> Memory::get_sprite_attributes() {
+  const u8* sprite_attrib_begin = &memory[0xfe00];
+  const u8* sprite_attrib_end = &memory[0xfea0];
+
+  return nonstd::span<const SpriteAttribute>(
+      reinterpret_cast<const SpriteAttribute*>(sprite_attrib_begin),
+      reinterpret_cast<const SpriteAttribute*>(sprite_attrib_end));
 }
 }  // namespace gb
--- src/memory.h
+++ src/memory.h
@@ -1,21 +1,44 @@
 #pragma once
+#include <algorithm>
 #include <array>
-#include "types.h"
+#include <functional>
+#include <utility>
+#include "nonstd/span.hpp"
+#include "sprite_attribute.h"
+#include "utils.h"
 
 namespace gb {
-struct Memory {
+class Memory {
   std::array<u8, 0x10000> memory;
 
+ public:
   template <typename T = u8>
-  inline const T* at(u16 addr) {
-    // assert(addr + (sizeof(T) - 1) < 0xffff);
-    return reinterpret_cast<const T*>(&memory.at(addr));
+  T at(u16 addr) {
+    if constexpr (sizeof(T) == 1) {
+      return memory.at(addr);
+    } else {
+      std::array<u8, sizeof(T)> bytes;
+      std::generate(bytes.begin(), bytes.end(),
+                    [this, addr, offset = sizeof(T) - 1]() mutable {
+                      return memory.at(addr + offset--);
+                    });
+      return convert_bytes<T>(bytes);
+    }
   }
+
+  nonstd::span<const u8> get_range(std::pair<u16, u16> range);
 
   void set(const u16& addr, const u8& val);
 
+  void reset();
+
+  void load_rom(const std::vector<u8>& data);
+
   void do_dma_transfer(const u8& val);
+
   const u8* get_input_register();
   void set_input_register(u8 val);
+
+  nonstd::span<const SpriteAttribute> get_sprite_attributes();
 };
 }  // namespace gb
