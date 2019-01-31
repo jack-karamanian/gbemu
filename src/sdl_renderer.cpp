43ecbdeba6cde3d004519a8adc427a0c2a75ce3d
Jack Karamanian <karamanian.jack@gmail.com>
2019-01-30 21:24:36 -0600

    implement SDL texture creation and rendering

--- src/sdl_renderer.cpp
+++ src/sdl_renderer.cpp
@@ -5,24 +5,46 @@ namespace gb {
 SdlRenderer::SdlRenderer(
     std::unique_ptr<SDL_Renderer, std::function<void(SDL_Renderer*)>> p_renderer)
     : renderer{std::move(p_renderer)},
-      texture{SDL_CreateTexture(renderer.get(),
-                                SDL_PIXELFORMAT_RGBA8888,
-                                SDL_TEXTUREACCESS_STREAMING,
-                                160,
-                                144),
-              [](auto t) { SDL_DestroyTexture(t); }},
-      format{SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888), [](auto f) { SDL_FreeFormat(f); } } 
+      format{SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888), sdl::SdlDeleter() } 
       {
-  if (!texture.get() || !format.get()) {
+        //SDL_SetRenderDrawBlendMode(renderer.get(), SDL_BLENDMODE_BLEND);
+  if (!format.get()) {
     std::cout << "SDL error :" << SDL_GetError() << std::endl;
   }
 }
 
-void SdlRenderer::draw_pixels(const std::vector<Pixel>& pixels) {
+Texture SdlRenderer::create_texture(int width, int height, bool blend) {
+  sdl::sdl_unique_ptr<SDL_Texture> texture{
+    SDL_CreateTexture(renderer.get(),
+                                SDL_PIXELFORMAT_RGBA8888,
+                                SDL_TEXTUREACCESS_STREAMING,
+                                width,
+                                height),
+      sdl::SdlDeleter()
+  };
+
+  if (blend) {
+    SDL_SetTextureBlendMode(texture.get(), SDL_BLENDMODE_BLEND);
+  }
+
+  int id = textures.size();
+  textures.emplace(id, std::move(texture));
+
+  return {id};
+}
+
+void SdlRenderer::clear() {
+  SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+  SDL_RenderClear(renderer.get());
+}
+
+void SdlRenderer::draw_pixels(Texture texture, const std::vector<Pixel>& pixels) {
+  SDL_Texture* sdl_texture = textures.at(texture.handle).get();
+
   Uint32* texture_pixels = nullptr;
   int pitch = -1;
 
-  if (SDL_LockTexture(texture.get(), nullptr, (void**)&texture_pixels,
+  if (SDL_LockTexture(sdl_texture, nullptr, (void**)&texture_pixels,
                       &pitch)) {
     std::cout << "SDL Error: " << SDL_GetError() << std::endl;
   }
@@ -35,13 +57,11 @@ void SdlRenderer::draw_pixels(const std::vector<Pixel>& pixels) {
     }
   }
 
-  SDL_UnlockTexture(texture.get());
+  SDL_UnlockTexture(sdl_texture);
+  SDL_RenderCopy(renderer.get(), sdl_texture, nullptr, nullptr);
+}
 
-  SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
-  SDL_RenderClear(renderer.get());
-
-  SDL_RenderCopy(renderer.get(), texture.get(), nullptr, nullptr);
-
+void SdlRenderer::present() {
   SDL_RenderPresent(renderer.get());
 }
 }  // namespace gb
