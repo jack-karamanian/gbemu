ca7fe9100ae30456875ca710cc6a2a3c674f663f
Jack Karamanian <karamanian.jack@gmail.com>
2019-02-11 22:20:38 -0600

    only write lcd state when it changes, return when to draw a frame

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -3,20 +3,30 @@
 #include "gpu.h"
 #include "lcd.h"
 #include "memory.h"
-#include "registers/lcd_stat.h"
 #include "registers/lyc.h"
 #include "types.h"
 
 namespace gb {
-void Lcd::update(unsigned int ticks) {
-  Registers::LcdStat lcd_stat{memory->at(Registers::LcdStat::Address)};
+void Lcd::check_scanlines(Registers::LcdStat& lcd_stat) const {
   const u8 lyc = memory->at(Registers::Lyc::Address);
+  if (lcd_stat.ly_equals_lyc_enabled() && scanlines == lyc) {
+    lcd_stat.set_ly_equals_lyc(true);
+    cpu->request_interrupt(Cpu::Interrupt::LcdStat);
+  } else {
+    lcd_stat.set_ly_equals_lyc(false);
+  }
+}
+bool Lcd::update(unsigned int ticks) {
+  bool draw_frame = false;
+  Registers::LcdStat lcd_stat{memory->at(Registers::LcdStat::Address)};
 
   lcd_ticks += ticks;
   switch (mode) {
     case 2: {
       if (lcd_ticks >= 80) {
+        check_scanlines(lcd_stat);
         mode = 3;
+        lcd_stat.set_mode(mode);
         lcd_ticks = 0;
       }
       break;
@@ -24,10 +34,12 @@ void Lcd::update(unsigned int ticks) {
     case 3: {
       if (lcd_ticks >= 172) {
         mode = 0;
+        lcd_stat.set_mode(mode);
         lcd_ticks = 0;
         if (lcd_stat.hblank_check_enabled()) {
           cpu->request_interrupt(Cpu::Interrupt::LcdStat);
         }
+        check_scanlines(lcd_stat);
         // Render scanline
         gpu->render_scanline(scanlines);
       }
@@ -36,17 +48,22 @@ void Lcd::update(unsigned int ticks) {
     case 0: {
       if (lcd_ticks >= 204) {
         scanlines++;
+        memory->set_ly(scanlines);
         lcd_ticks = 0;
+        check_scanlines(lcd_stat);
         if (scanlines >= 144) {
           mode = 1;
+          lcd_stat.set_mode(mode);
           if (lcd_stat.vblank_check_enabled() || lcd_stat.oam_check_enabled()) {
             cpu->request_interrupt(Cpu::Interrupt::LcdStat);
           }
           // Render image
           gpu->render();
           cpu->request_interrupt(Cpu::Interrupt::VBlank);
+          draw_frame = true;
         } else {
           mode = 2;
+          lcd_stat.set_mode(mode);
           if (lcd_stat.oam_check_enabled()) {
             cpu->request_interrupt(Cpu::Interrupt::LcdStat);
           }
@@ -55,33 +72,27 @@ void Lcd::update(unsigned int ticks) {
       break;
     }
     case 1: {
-      if (lcd_ticks > 456) {
+      if (lcd_ticks >= 456) {
         scanlines++;
         lcd_ticks = 0;
 
         if (scanlines > 153) {
           mode = 2;
+          lcd_stat.set_mode(mode);
           scanlines = 0;
+          check_scanlines(lcd_stat);
           if (lcd_stat.oam_check_enabled()) {
             cpu->request_interrupt(Cpu::Interrupt::LcdStat);
           }
         }
+        memory->set_ly(scanlines);
       }
       break;
     }
   }
 
-  if (lcd_stat.ly_equals_lyc_enabled() && scanlines == lyc) {
-    lcd_stat.set_ly_equals_lyc(true);
-    cpu->request_interrupt(Cpu::Interrupt::LcdStat);
-  } else {
-    lcd_stat.set_ly_equals_lyc(false);
-  }
-
-  lcd_stat.set_mode(mode);
-
-  memory->set(Registers::LcdStat::Address, lcd_stat.get_value());
-  memory->set(LCDC_Y_COORD, scanlines);
+  memory->set_lcd_stat(lcd_stat.get_value());
+  return draw_frame;
 }
 void Lcd::write_lcd_stat() const {
   // memory->set(LCD_STAT_REGISTER, mode);
--- src/lcd.h
+++ src/lcd.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "registers/lcd_stat.h"
 
 #define LCD_STAT_REGISTER 0xff41
 #define LCDC_Y_COORD 0xff44
@@ -18,14 +19,16 @@ class Lcd {
   Memory* memory;
   Gpu* gpu;
 
-  int mode = 2;
+  int mode = 0;
   int scanlines = 0;
   unsigned int lcd_ticks = 0;
   void write_lcd_stat() const;
 
+  void check_scanlines(Registers::LcdStat& lcd_stat) const;
+
  public:
   Lcd(Cpu& cpu, Memory& memory, Gpu& gpu)
       : cpu{&cpu}, memory{&memory}, gpu{&gpu} {}
-  void update(unsigned int ticks);
+  bool update(unsigned int ticks);
 };
 }  // namespace gb
