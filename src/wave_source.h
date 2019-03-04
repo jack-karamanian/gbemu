337be5591a96d85cf2f8544d42008fbe6ec71c10
Jack Karamanian <karamanian.jack@gmail.com>
2019-03-03 20:21:49 -0600

    separate the source of wave channel samples

---
+++ src/wave_source.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <nonstd/span.hpp>
+#include "types.h"
+
+namespace gb {
+class WaveSource {
+  bool enabled = false;
+  int timer_base = 0;
+  int timer = 0;
+
+  int wave_progress = 0;
+
+  nonstd::span<const u8> wave_buffer;
+
+ public:
+  WaveSource(nonstd::span<const u8> buffer) : wave_buffer{std::move(buffer)} {}
+
+  void set_timer_base(int frequency) { timer_base = (2048 - frequency) * 2; }
+
+  void enable();
+
+  u8 get_sample();
+
+  u8 update();
+};
+}  // namespace gb
