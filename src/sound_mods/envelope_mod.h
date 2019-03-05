5dbb6592b1f3f3fcb8ebdf1d2412870e62cee934
Jack Karamanian <karamanian.jack@gmail.com>
2019-03-04 20:45:27 -0600

    add commands to envelope mod

--- src/sound_mods/envelope_mod.h
+++ src/sound_mods/envelope_mod.h
@@ -1,6 +1,19 @@
 #pragma once
 
 namespace gb {
+
+struct SetStartingVolumeCommand {
+  int starting_volume = 0;
+};
+
+struct SetIncreaseVolumeCommand {
+  bool increase_volume = false;
+};
+
+struct SetPeriodCommand {
+  int period = 0;
+};
+
 class EnvelopeMod {
   int period = 0;
   int timer = 0;
@@ -20,5 +33,15 @@ class EnvelopeMod {
   void clock(int step);
 
   float update(float input_volume) const;
+
+  void dispatch(SetStartingVolumeCommand command) {
+    set_starting_volume(command.starting_volume);
+  }
+
+  void dispatch(SetIncreaseVolumeCommand command) {
+    set_increase_volume(command.increase_volume);
+  }
+
+  void dispatch(SetPeriodCommand command) { set_period(command.period); }
 };
 }  // namespace gb
