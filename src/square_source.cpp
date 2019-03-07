bd0f50e8cd2e90059abb170bf12b10eeb9a1abdb
Jack Karamanian <karamanian.jack@gmail.com>
2019-03-07 00:04:36 -0600

    don't use arrays to store duty cycles

--- src/square_source.cpp
+++ src/square_source.cpp
@@ -5,12 +5,11 @@ u8 SquareSource::update() {
   if (--timer <= 0) {
     timer = timer_base;
     wave_progress++;
+    if (wave_progress > 7) {
+      wave_progress = 0;
+    }
   }
 
-  if (wave_progress > 7) {
-    wave_progress = 0;
-  }
-
-  return DUTY_CYCLES.at(duty_cycle).at(wave_progress) ? 15 : 0;
+  return (duty_cycle & (1 << wave_progress)) != 0 ? 15 : 0;
 }
 }  // namespace gb
--- src/square_source.h
+++ src/square_source.h
@@ -4,12 +4,8 @@
 
 namespace gb {
 class SquareSource {
-  static constexpr std::array<std::array<bool, 8>, 5> DUTY_CYCLES = {
-      {{false, false, false, false, false, false, false, true},
-       {true, false, false, false, false, false, false, true},
-       {true, false, false, false, false, true, true, true},
-       {false, true, true, true, true, true, true, false},
-       {false, false, false, false, false, false, false, false}}};
+  static constexpr std::array<u8, 5> DUTY_CYCLES = {0b00000001, 0b10000001,
+                                                    0b10000111, 0b01111110, 0};
 
   // Timer/frequency
   int timer_base = 0;
@@ -23,7 +19,7 @@ class SquareSource {
   void enable() { timer = timer_base; }
   void set_timer_base(int value) { timer_base = (2048 - value) * 4; }
 
-  void set_duty_cycle(int value) { duty_cycle = value; }
+  void set_duty_cycle(int value) { duty_cycle = DUTY_CYCLES.at(value); }
 
   u8 update();
 };
