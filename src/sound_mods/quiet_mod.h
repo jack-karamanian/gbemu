352da369e24e73b46ca60c6dc799c2a8db399c60
Jack Karamanian <karamanian.jack@gmail.com>
2019-03-07 00:03:38 -0600

    avoid double -> float conversion

--- src/sound_mods/quiet_mod.h
+++ src/sound_mods/quiet_mod.h
@@ -1,7 +1,7 @@
 #pragma once
 
 struct QuietMod {
-  float update(float volume) const { return (volume / 16.0f) * 0.01; }
+  float update(float volume) const { return (volume / 16.0f) * 0.01f; }
   void enable() {}
   void clock(int step) { static_cast<void>(step); }
 };
