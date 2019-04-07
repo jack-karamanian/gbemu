4d17096886777a0c363457ec239c538f959295b5
Jack Karamanian <karamanian.jack@gmail.com>
2019-04-06 20:22:23 -0500

    put constants in namespace gb

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,9 @@
 #pragma once
 
+namespace gb {
 constexpr int SCREEN_WIDTH = 160;
 constexpr int SCREEN_HEIGHT = 144;
 constexpr int DISPLAY_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
 
 constexpr int TILE_SIZE = 8;
+}  // namespace gb
