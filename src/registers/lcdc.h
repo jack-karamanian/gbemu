0291d966dda2cc76943f0a97412f1a77f4441db0
Jack Karamanian <karamanian.jack@gmail.com>
2019-04-30 19:44:25 -0500

    use nodiscard in Lcdc

--- src/registers/lcdc.h
+++ src/registers/lcdc.h
@@ -13,10 +13,10 @@ class Lcdc {
   static constexpr u16 Address = 0xff40;
 
   // Bit 7
-  bool controller_on() const { return value & 0x80; }
+  [[nodiscard]] bool controller_on() const { return value & 0x80; }
 
   // Bit 6
-  std::pair<u16, u16> window_tile_map_range() const {
+  [[nodiscard]] std::pair<u16, u16> window_tile_map_range() const {
     bool high_area = value & 0x40;
 
     if (high_area) {
@@ -27,10 +27,10 @@ class Lcdc {
   }
 
   // Bit 5
-  bool window_on() const { return value & 0x20; }
+  [[nodiscard]] bool window_on() const { return value & 0x20; }
 
   // Bit 4
-  std::pair<u16, u16> bg_tile_data_range() const {
+  [[nodiscard]] std::pair<u16, u16> bg_tile_data_range() const {
     bool selection = value & 0x10;
 
     if (selection) {
@@ -41,7 +41,7 @@ class Lcdc {
   }
 
   // Bit 3
-  std::pair<u16, u16> bg_tile_map_range() const {
+  [[nodiscard]] std::pair<u16, u16> bg_tile_map_range() const {
     bool selection = value & 0x8;
 
     if (selection) {
@@ -52,26 +52,26 @@ class Lcdc {
   }
 
   // Bit 2
-  SpriteMode sprite_size() const {
+  [[nodiscard]] SpriteMode sprite_size() const {
     bool eight_by_sixteen = value & 0x4;
     return eight_by_sixteen ? SpriteMode::EightBySixteen
                             : SpriteMode::EightByEight;
   }
 
   // Bit 1
-  bool obj_on() const { return value & 0x2; }
+  [[nodiscard]] bool obj_on() const { return value & 0x2; }
 
   // Bit 0
-  bool bg_on() const { return value & 0x1; }
+  [[nodiscard]] bool bg_on() const { return value & 0x1; }
 
   // Extras
-  u16 bg_tile_data_base() const {
+  [[nodiscard]] u16 bg_tile_data_base() const {
     const auto [addr, _] = bg_tile_data_range();
 
     return addr == 0x8000 ? addr : 0x8800;
   }
 
-  bool is_tile_map_signed() const { return bg_tile_data_base() == 0x8800; }
+  [[nodiscard]] bool is_tile_map_signed() const { return bg_tile_data_base() == 0x8800; }
 };
 }  // namespace Registers
 }  // namespace gb
