6b44a479cc83fc8d55cc16f92a61d60cb11f728f
Jack Karamanian <karamanian.jack@gmail.com>
2019-05-28 19:33:54 -0500

    add more utility functions

--- core/src/utils.h
+++ core/src/utils.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <array>
+#include <nonstd/span.hpp>
 #include <optional>
 #include <type_traits>
 #include "types.h"
+
 namespace gb {
+
 template <typename... Args>
 [[nodiscard]] constexpr u8 get_bits(Args... args) {
   static_assert((std::is_same_v<Args, bool> && ...));
@@ -23,7 +26,7 @@ template <typename... Args>
 
 template <typename T>
 [[nodiscard]] constexpr T convert_bytes(
-    const std::array<u8, sizeof(T)>& bytes) {
+    nonstd::span<const u8, sizeof(T)> bytes) {
   T res{0};
 
   int shift = (sizeof(T) * 8) - 8;
@@ -31,6 +34,32 @@ template <typename T>
   for (u8 byte : bytes) {
     res |= (byte << shift);
     shift -= 8;
+  }
+
+  return res;
+}
+
+template <typename T>
+[[nodiscard]] constexpr T convert_bytes_endian(
+    nonstd::span<const u8, sizeof(T)> bytes) {
+  T res{0};
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+  for (int i = sizeof(T) - 1; i >= 0; --i) {
+    res |= bytes[i] << (i * 8);
+  }
+#else
+#error "Big endian is not implemented"
+#endif
+  return res;
+}
+
+template <typename T>
+[[nodiscard]] constexpr std::array<u8, sizeof(T)> to_bytes(T val) {
+  std::array<u8, sizeof(T)> res;
+
+  for (std::size_t i = 0; i < sizeof(T); ++i) {
+    const std::size_t shift = i * 8;
+    res[i] = (val & (0xff << shift)) >> shift;
   }
 
   return res;
@@ -44,6 +73,16 @@ void for_static_impl(Func&& f, std::index_sequence<I...>) {
 template <std::size_t I, typename Func>
 void for_static(Func&& f) {
   for_static_impl(f, std::make_index_sequence<I>());
+}
+
+template <std::size_t Offset, std::size_t... I>
+constexpr auto tuple_from_range_impl(std::index_sequence<I...>) {
+  return std::make_tuple(std::integral_constant<std::size_t, Offset + I>{}...);
+}
+template <std::size_t Begin, std::size_t End>
+constexpr auto tuple_from_range() {
+  return tuple_from_range_impl<Begin>(
+      std::make_index_sequence<End - Begin + 1>());
 }
 
 template <typename T>
@@ -74,6 +113,41 @@ template <typename T>
 template <typename T, typename U>
 [[nodiscard]] constexpr T increment_bits(T value, U mask) {
   return (value & ~mask) | (((value & mask) + 1) & mask);
+}
+
+template <typename T, typename... Args>
+[[nodiscard]] constexpr T set_bits(const Args... bits) {
+  return ((1 << bits) | ...);
+}
+
+template <typename Itr, typename Func>
+[[nodiscard]] constexpr Itr constexpr_find(Itr begin, Itr end, Func func) {
+  for (; begin != end; ++begin) {
+    if (func(*begin)) {
+      return begin;
+    }
+  }
+  return end;
+}
+
+[[nodiscard]] constexpr u32 rotate_right(u32 val, u32 amount) {
+  constexpr std::size_t end_shift = sizeof(val) * 8 - 1;
+
+  for (unsigned i = 0; i < amount; ++i) {
+    val = ((val & 1) << end_shift) | (val >> 1);
+  }
+
+  return val;
+}
+
+[[nodiscard]] constexpr u32 arithmetic_shift_right(u32 val, u32 amount) {
+  constexpr std::size_t end_shift = sizeof(val) * 8 - 1;
+  return (val & (1 << end_shift)) | (val >> amount);
+}
+
+template <int from, int to, typename T>
+[[nodiscard]] constexpr T convert_space(T value) {
+  return (value * to) / from;
 }
 
 }  // namespace gb
