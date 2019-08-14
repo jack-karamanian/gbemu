347105fcfdba78ba3f79048a2e59a755599342c7
Jack Karamanian <karamanian.jack@gmail.com>
2019-08-13 22:50:28 -0500

    add constructors for Color

--- core/src/color.h
+++ core/src/color.h
@@ -6,5 +6,9 @@ struct Color {
   u8 g = 0;
   u8 b = 0;
   u8 a = 0;
+
+  constexpr Color(u8 r_, u8 g_, u8 b_, u8 a_) : r{r_}, g{g_}, b{b_}, a{a_} {}
+  constexpr Color(u8 r_, u8 g_, u8 b_) : r{r_}, g{g_}, b{b_} {}
+  constexpr Color() = default;
 };
 }  // namespace gb
