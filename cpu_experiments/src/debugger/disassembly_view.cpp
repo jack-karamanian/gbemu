06177fbf01a72457473e4f6b2fe94fdaeff56e1e
Jack Karamanian <karamanian.jack@gmail.com>
2019-09-13 18:31:43 -0500

    add goto address button to DisassemblyView

--- cpu_experiments/src/debugger/disassembly_view.cpp
+++ cpu_experiments/src/debugger/disassembly_view.cpp
@@ -1,4 +1,6 @@
+
 #include "disassembly_view.h"
+#include <cstdlib>
 #include "gba/cpu.h"
 #include "imgui.h"
 
@@ -6,21 +8,41 @@ namespace gb::advance {
 void DisassemblyView::render(gb::u32 base,
                              const Cpu& cpu,
                              const DisassemblyInfo& disassembly_info) {
+  static std::array<char, 9> m_number_buffer;
   ImGui::Begin(m_name);
+
+  std::optional<u32> target_addr;
+
+  const auto go_to_address = [&](u32 address) { target_addr = address; };
+
+  ImGui::InputTextWithHint("", "Address", m_number_buffer.data(), 9);
+  ImGui::SameLine();
+  if (ImGui::Button("Goto")) {
+    char* end = nullptr;
+    const u32 addr = std::strtol(m_number_buffer.begin(), &end, 16);
+    go_to_address(addr - base);
+  }
+
+  ImGui::BeginChild("Inner");
   ImGuiListClipper clipper(disassembly_info.disassembly.size(),
                            ImGui::GetTextLineHeightWithSpacing());
   const gb::u32 offset =
       (cpu.reg(gb::advance::Register::R15) - cpu.prefetch_offset()) - base;
 
   if (offset != m_prev_offset) {
-    const auto index = disassembly_info.addr_to_index.find(offset);
+    go_to_address(offset);
+    m_prev_offset = offset;
+  }
+
+  if (target_addr) {
+    const auto index = disassembly_info.addr_to_index.find(*target_addr);
     if (index != disassembly_info.addr_to_index.end()) {
       const float adjusted_offset =
           ImGui::GetCursorStartPos().y +
           index->second * (ImGui::GetTextLineHeightWithSpacing());
       ImGui::SetScrollFromPosY(adjusted_offset);
     }
-    m_prev_offset = offset;
+    target_addr = {};
   }
 
   while (clipper.Step()) {
@@ -34,6 +56,7 @@ void DisassemblyView::render(gb::u32 base,
       }
     }
   }
+  ImGui::EndChild();
   ImGui::End();
 }
 }  // namespace gb::advance
