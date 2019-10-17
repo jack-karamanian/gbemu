6781d8e7baa60aa53f32c6d2d8e27219c81ed697
Jack Karamanian <karamanian.jack@gmail.com>
2019-10-17 18:28:30 -0500

    move sprite related structures to header file

--- core/src/gba/gpu.h
+++ core/src/gba/gpu.h
@@ -57,8 +57,7 @@ class Dispcnt : public Integer<u16> {
 
 class Bgcnt : public Integer<u16> {
  public:
-  Bgcnt(Gpu& gpu, Dispcnt::BackgroundLayer layer)
-      : Integer::Integer{0}, m_gpu{&gpu}, m_layer{layer} {}
+  Bgcnt(Gpu& gpu) : Integer::Integer{0}, m_gpu{&gpu} {}
 
   void on_after_write() const;
 
@@ -113,12 +112,102 @@ class Bgcnt : public Integer<u16> {
 
  private:
   Gpu* m_gpu;
-  Dispcnt::BackgroundLayer m_layer;
 };
 
 class Bldcnt : public Integer<u16> {
  public:
   using Integer::Integer;
+};
+
+class ObjAttribute0 : public Integer<u16> {
+ public:
+  using Integer::Integer;
+
+  [[nodiscard]] unsigned int y() const { return m_value & 0xff; }
+
+  enum class Mode {
+    Normal,
+    Affine,
+    Disable,
+    AffineDoubleRendering,
+  };
+
+  [[nodiscard]] Mode mode() const {
+    return static_cast<Mode>((m_value >> 8) & 0b11);
+  }
+
+  enum class GfxMode {
+    Normal,
+    AlphaBlending,
+    Window,
+    Forbidden,
+  };
+
+  [[nodiscard]] GfxMode gfx_mode() const {
+    return static_cast<GfxMode>((m_value >> 10) & 0b11);
+  }
+
+  [[nodiscard]] bool disable() const { return test_bit(9); }
+
+  [[nodiscard]] bool mosaic() const { return test_bit(12); }
+
+  [[nodiscard]] unsigned int bits_per_pixel() const {
+    return test_bit(13) ? 8 : 4;
+  }
+
+  enum class Shape {
+    Square = 0,
+    Horizontal = 1,
+    Vertical = 2,
+  };
+
+  [[nodiscard]] Shape shape() const {
+    return static_cast<Shape>((m_value >> 14) & 0b11);
+  }
+};
+
+class ObjAttribute1 : public Integer<u16> {
+ public:
+  using Integer::Integer;
+  [[nodiscard]] unsigned int x() const { return m_value & 0b1'1111'1111; }
+
+  [[nodiscard]] bool horizontal_flip() const { return test_bit(12); }
+
+  [[nodiscard]] bool vertical_flip() const { return test_bit(13); }
+
+  [[nodiscard]] unsigned int obj_size() const { return (m_value >> 14) & 0b11; }
+};
+
+class ObjAttribute2 : public Integer<u16> {
+ public:
+  using Integer::Integer;
+  [[nodiscard]] unsigned int tile_id() const {
+    return m_value & 0b11'1111'1111;
+  }
+
+  [[nodiscard]] unsigned int priority() const { return (m_value >> 10) & 0b11; }
+
+  [[nodiscard]] unsigned int palette_bank() const {
+    return (m_value >> 12) & 0b1111;
+  }
+};
+
+struct Sprite {
+  ObjAttribute0 attrib0;
+  ObjAttribute1 attrib1;
+  ObjAttribute2 attrib2;
+
+  Sprite(u16 attrib0_value, u16 attrib1_value, u16 attrib2_value)
+      : attrib0{attrib0_value},
+        attrib1{attrib1_value},
+        attrib2{attrib2_value} {}
+
+  Sprite() : attrib0{0}, attrib1{0}, attrib2{0} {}
+};
+
+struct Sprites {
+  std::array<Sprite, 128> sprites{};
+  unsigned int num_active_sprites = 0;
 };
 
 class Gpu {
@@ -134,9 +223,11 @@ class Gpu {
     Bgcnt control;
     Dispcnt::BackgroundLayer layer;
     Vec2<u16> scroll{0, 0};
+    // std::vector<Color> framebuffer;
 
     Background(Gpu& gpu, Dispcnt::BackgroundLayer l)
-        : control{gpu, l}, layer{l} {}
+        : control{gpu},
+          layer{l} /*, framebuffer(ScreenWidth * ScreenHeight)*/ {}
   };
 
   Gpu(Mmu& mmu)
