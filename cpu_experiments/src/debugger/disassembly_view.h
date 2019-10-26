a5c8412a0d2779664a86e2ff370077badb1bc073
Jack Karamanian <karamanian.jack@gmail.com>
2019-10-26 17:47:24 -0500

    make DisassemblyView constructor constexpr

--- cpu_experiments/src/debugger/disassembly_view.h
+++ cpu_experiments/src/debugger/disassembly_view.h
@@ -13,8 +13,7 @@ struct DisassemblyInfo {
 
 class DisassemblyView {
  public:
-  DisassemblyView(const char* name, u32 instruction_size)
-      : m_name{name}, m_instruction_size{instruction_size} {}
+  constexpr explicit DisassemblyView(const char* name) : m_name{name} {}
   void render(u32 base,
               const Cpu& cpu,
               const DisassemblyInfo& disassembly_info);
@@ -22,9 +21,6 @@ class DisassemblyView {
  private:
   const char* m_name;
   u32 m_prev_offset = 0;
-  u32 m_instruction_size;
-  std::array<char, 9> m_number_buffer;
-  folly::F14FastMap<unsigned int, experiments::DisassemblyEntry>
-      m_address_to_disassembly;
+  std::array<char, 9> m_number_buffer{};
 };
 }  // namespace gb::advance
