90d6421512405fda6747a86837a19d4754d8b59c
Jack Karamanian <karamanian.jack@gmail.com>
2020-04-17 19:20:39 -0500

    StaticVector: implement properly using aligned byte arrays

--- core/src/static_vector.h
+++ core/src/static_vector.h
@@ -13,9 +13,13 @@ class StaticVector {
 
   constexpr StaticVector() = default;
 
-  [[nodiscard]] constexpr iterator begin() noexcept { return &m_data[0]; }
+  [[nodiscard]] constexpr iterator begin() noexcept {
+    return std::launder(reinterpret_cast<T*>(&m_data[0]));
+  }
 
-  [[nodiscard]] constexpr iterator end() noexcept { return &m_data[m_size]; }
+  [[nodiscard]] constexpr iterator end() noexcept {
+    return std::launder(reinterpret_cast<T*>(&m_data[m_size]));
+  }
 
   [[nodiscard]] constexpr reverse_iterator rbegin() {
     return std::make_reverse_iterator(end());
@@ -26,18 +30,18 @@ class StaticVector {
   }
 
   [[nodiscard]] constexpr const_iterator cbegin() const noexcept {
-    return &m_data[0];
+    return std::launder(reinterpret_cast<const T*>(&m_data[0]));
   }
 
   [[nodiscard]] constexpr const_iterator cend() const noexcept {
-    return &m_data[m_size];
+    return std::launder(reinterpret_cast<const T*>(&m_data[m_size]));
   }
 
   constexpr void push_back(const T& item) {
     if (m_size >= Capacity) {
       throw std::runtime_error("vector full");
     }
-    m_data[m_size] = item;
+    new (&m_data[m_size]) T(item);
     ++m_size;
   }
 
@@ -53,14 +57,25 @@ class StaticVector {
 
   [[nodiscard]] constexpr std::size_t size() const noexcept { return m_size; }
 
-  [[nodiscard]] constexpr reference back() { return m_data[m_size - 1]; }
+  [[nodiscard]] constexpr reference back() {
+    return *std::launder(reinterpret_cast<T*>(&m_data[m_size - 1]));
+  }
   [[nodiscard]] constexpr const_reference back() const {
-    return m_data[m_size - 1];
+    return *std::launder(reinterpret_cast<const T*>(&m_data[m_size - 1]));
   }
 
+  [[nodiscard]] constexpr bool is_empty() const noexcept { return size() == 0; }
+
   template <typename... Args>
-  constexpr void emplace_back(Args... args) {
-    m_data[m_size++] = T{std::forward<Args>(args)...};
+  constexpr void emplace_back(Args&&... args) {
+    if (m_size >= Capacity) {
+      throw std::runtime_error("no space");
+    }
+    if constexpr (std::is_aggregate_v<T>) {
+      new (&m_data[m_size++]) T{std::forward<Args>(args)...};
+    } else {
+      new (&m_data[m_size++]) T(std::forward<Args>(args)...);
+    }
   }
 
   constexpr void clear() noexcept { m_size = 0; }
@@ -69,11 +84,11 @@ class StaticVector {
     return Capacity;
   }
 
-  constexpr reference operator[](std::size_t index) { return m_data[index]; }
-
-  // T* data() {}
+  constexpr reference operator[](std::size_t index) {
+    return *std::launder(reinterpret_cast<T*>(&m_data[index]));
+  }
 
  private:
-  std::array<T, Capacity> m_data{};
   std::size_t m_size = 0;
+  std::array<std::aligned_storage_t<sizeof(T), alignof(T)>, Capacity> m_data;
 };
